c3258d85a6e5f9076b74077fecd0f50be5938052
Jonatas Ribeiro <jonatas@prottaribeiro.com>
2023-05-11 16:05:19 +0100

    crating calculator app

---
+++ calculatorApp/src/App.jsx
@@ -0,0 +1,19 @@
+import './App.css'
+
+function App() {
+
+  return (
+    <>
+      
+      <h1>Calculator App</h1>
+      <div className="card">
+        
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
